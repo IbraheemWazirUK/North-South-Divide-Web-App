8e07e927001c55a77ff78a7d50bd95300935d233
Václav Pavlíček <vaclav@pavlicek.cz>
2021-09-19 17:04:16 +0100

    add storing data to database

--- src/App.js
+++ src/App.js
@@ -8,18 +8,40 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {loading: true, drawingMissing: true};
+        this.state = {loading: true, drawingMissing: true, item: {message: ""}, token: "" };
     }
 
     componentDidMount() {
         Auth.currentSession().then(async authConfig => {
-            const result = await axios.get('https://l6m6isx62e.execute-api.eu-west-2.amazonaws.com/prod/drawing', {headers: {authorization: authConfig.getIdToken().getJwtToken()}});
+            const token = authConfig.getIdToken().getJwtToken();
+            const result = await axios.get('https://l6m6isx62e.execute-api.eu-west-2.amazonaws.com/prod/drawing', {headers: {authorization: token}});
+
             if (result && result.data && result.data.message === "User's drawing is not in the database.") {
-                this.setState({loading: false});
+                this.setState({loading: false, token});
             } else {
-                this.setState({loading: false, drawingMissing: false});
+                const parsedData = JSON.parse(result.data.data);
+                this.setState({loading: false, drawingMissing: false, token, item: parsedData});
             }
         });
+    }
+
+    handleInputChanged(event) {
+        const item = {...(this.state.data)};
+        item.message = event.target.value;
+        this.setState({item});
+    }
+
+    async handleDatabaseSave() {
+        const response = await axios.post('https://l6m6isx62e.execute-api.eu-west-2.amazonaws.com/prod/drawing', this.state.item, {
+            headers: {
+                authorization: this.state.token
+            }
+        });
+        if (response && response.data && response.data.message === "User's drawing inserted to the database.") {
+            this.setState({ drawingMissing: false });
+        } else {
+            console.error("Unknown error occurred!");
+        }
     }
 
     render() {
@@ -29,7 +51,14 @@ class App extends React.Component {
                 {this.state.loading ?
                     <b>Loading...</b> :
                     this.state.drawingMissing ?
-                        <b>Drawing is missing</b> : <b>Drawing is present</b>
+                        <div>
+                            <input type="text" onChange={this.handleInputChanged.bind(this)} />
+                            <button onClick={this.handleDatabaseSave.bind(this)}>Save</button>
+                        </div>
+                        :
+                        <div>
+                            <p><b>Data stored in DB:</b> {this.state.item.message}</p>
+                        </div>
                 }
             </div>
         );
